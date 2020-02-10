c80b05ca57ec15996f30003c32b208dbad315e8a
Shruti Srivastava <shruti_srivastava86@yahoo.com>
2020-02-10 22:16:02 +0530

    Updated utils

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # aubergine
 test
 
-Assumptions: Compressed URL is done through Lambda on S3 buckets
+Assumptions: Compressed URL is done through Lambda on S3 buckets and strored in -resized bucket
--- images/utils.py
+++ images/utils.py
@@ -2,7 +2,8 @@ from models import ImageUrl
 
 def save_to_db(urls_list):
     for url_item in urls_list:
-        new_url=
+        a,b=url_item.split('.s3')
+        new_url=str(a)+'-resized'+str(b)
         image_row=ImageUrl.objects.create(
             original_url=url_item,
             compressed_url=new_url
